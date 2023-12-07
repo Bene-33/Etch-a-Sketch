f2e7bc92d97a1ae568de1924e57b123137d86e33
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-07 14:28:19 +0100

    function to creat 16x16 divs

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const grid = document.querySelector('#grid');
-let pixels = document.createElement('div');
-
-pixels.classList.add('pixels');
-
-grid.appendChild(pixels);
 
 
+function creatPixels(rows, columns){
+    for(let i = 0; i < (rows * columns); i++){
+        let pixels = document.createElement('div');
+        pixels.classList.add('pixels');
+        grid.appendChild(pixels);
+    };
+};
+
+creatPixels(16,16);
