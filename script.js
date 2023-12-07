b3aeb9c9b70b23d9601ee02cc41e1ec1e82a8e1f
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-07 19:47:45 +0100

    commend code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('#grid');
 
+//creat grid rows x columns
 function creatPixels(rows, columns){
     for(let i = 0; i < (rows * columns); i++){
         let pixels = document.createElement('div');
@@ -11,6 +12,7 @@ creatPixels(16,16);
 
 const draw = document.querySelectorAll('.pixels');
 
+// paint with the mouse 
 for (let i = 0; i < draw.length; i++) {
     draw[i].addEventListener('mouseover', () => {
         draw[i].classList.toggle('black');
