35a3c6792b87af1b7720c6dc44a7fef7916067f1
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-08 13:47:56 +0100

    added event listener at button to enter new grid size, need to fix grid layout

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const grid = document.querySelector('#grid');
-const brush = document.querySelector('#brush')
+const brush = document.querySelector('#brush');
+const button = document.querySelector('#brush');
 let columns = 16;
 let rows = columns;
 
@@ -20,4 +21,9 @@ for (let i = 0; i < draw.length; i++) {
     draw[i].addEventListener('mouseover', () => {
         draw[i].classList.toggle('black');
     })
-}
+};
+
+button.addEventListener('click', () => {
+    return columns = prompt('enter new grid size'),
+    creatPixels(rows, columns)
+});
