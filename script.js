a76fee8cd48a13503e29deed3cb8e2d9d5ef4635
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2023-12-11 16:09:40 +0100

    set color array for later use to be functionable

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const brush = document.querySelector('#brush');
 const button = document.querySelector('#brush');
 let columns = 16;
 let rows = columns;
-let color = 'black';
+let color = ['black'];
 
 //creat grid rows x columns
 function creatPixels(rows, columns){
@@ -13,21 +13,19 @@ function creatPixels(rows, columns){
         grid.appendChild(pixels);
     };
 };
+
 creatPixels(rows,columns);
 
+// paint/draw with the mouse 
 const draw = document.querySelectorAll('.pixels');
-
-// paint with the mouse 
 for (let i = 0; i < draw.length; i++) {
     draw[i].addEventListener('mouseover', () => {
-        draw[i].classList.toggle(color);
+        draw[i].classList.add(color[0]);
     })
 };
 
 //set up grid/brush size 
 button.addEventListener('click', () => {
-    columns = prompt('enter new grid size');
-    if (columns >= 100){columns = 100};
-    creatPixels(rows,columns);
+    columnsNew = prompt('enter new grid size');
+    if (columnsNew >= 100){columnsNew = 100};
 });
-
